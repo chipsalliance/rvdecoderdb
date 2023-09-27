0675b8a4daa12817ad39c906778f4daf0d2d7c03
Jiuyang Liu <liu@jiuyang.me>
2023-09-27 14:03:08 +0800

    [test] add json output string test

--- rvdecoderdbtest/jvm/src/Test.scala
+++ rvdecoderdbtest/jvm/src/Test.scala
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023 Jiuyang Liu <liu@jiuyang.me>
 
-package org.chipsalliance.rvdecoderdb.test
-
-object Test extends App {
+object printall extends App {
   org.chipsalliance.rvdecoderdb.fromFile(os.pwd / "rvdecoderdbtest" / "jvm"  / "riscv-opcodes").foreach(println)
+}
+
+object json extends App {
+  org.chipsalliance.rvdecoderdb.fromFile(os.pwd / "rvdecoderdbtest" / "jvm"  / "riscv-opcodes").foreach(i => println(upickle.default.write(i)))
 }
