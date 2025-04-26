956bc8d7482750b17e1d77cabf38343d015e8d57
Clo91eaf <Clo91eaf@qq.com>
2025-04-26 15:40:21 +0800

    [difftest] create difftest log with spike
    Signed-off-by: Clo91eaf <Clo91eaf@qq.com>

---
+++ nix/difftest/spike.nix
@@ -0,0 +1,47 @@
+{
+  stdenv,
+  dtc,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation {
+  version = "unstable-2024-07-03";
+  pname = "spike";
+
+  # env.cmakeConfig = ''
+  #   add_library(libspike STATIC IMPORTED GLOBAL)
+  #   set_target_properties(libspike PROPERTIES
+  #     IMPORTED_LOCATION "${placeholder "out"}/lib/libriscv.a")
+  #   target_include_directories(libspike AFTER INTERFACE
+  #     "${placeholder "out"}/include"
+  #     "${placeholder "out"}/include/riscv"
+  #     "${placeholder "out"}/include/fesvr"
+  #     "${placeholder "out"}/include/softfloat"
+  #   )
+  # '';
+  nativeBuildInputs = [ dtc ];
+  enableParallelBuilding = true;
+  separateDebugInfo = true;
+  src = fetchFromGitHub {
+    owner = "riscv";
+    repo = "riscv-isa-sim";
+    rev = "4a2da916671d49d9ab82f702f50995c19110c2a3";
+    hash = "sha256-c+yYuz2Z2/MwGmHYcv/gPIJQluzBjw8uUlOXsf9Bz28=";
+  };
+  configureFlags = [
+    "--enable-commitlog"
+  ];
+
+  # installPhase = ''
+  #   runHook preInstall
+  #   mkdir -p $out/include/{riscv,fesvr,softfloat} $out/lib $out/lib/cmake/libspike
+  #   cp riscv/*.h $out/include/riscv
+  #   cp fesvr/*.h $out/include/fesvr
+  #   cp softfloat/*.h $out/include/softfloat
+  #   cp config.h $out/include
+  #   cp *.so $out/lib
+  #   cp *.a $out/lib
+  #   echo "$cmakeConfig" > $out/lib/cmake/libspike/libspike-config.cmake
+  #   runHook postInstall
+  # '';
+}
