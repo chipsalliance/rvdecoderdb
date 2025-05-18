bcf6fd010ac3352ed1b6dc71377c1191270b7d46
Avimitin <dev@avimit.in>
2025-05-18 12:51:59 +0800

    [boat] allow 16 radix value for memory size param

--- sail-impl/boat/emulator/src/main.rs
+++ sail-impl/boat/emulator/src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
 use sail_ffi::{SimulationException, Simulator, SimulatorParams};
+use std::fmt::Display;
+use std::str::FromStr;
 use tracing::{event, Level};
-use tracing_subscriber::{filter::LevelFilter, EnvFilter, FmtSubscriber};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -12,8 +14,8 @@ struct Args {
     elf_path: String,
 
     /// Number of times to greet
-    #[arg(short, long, default_value_t = 0xa000_0000)]
-    memory_size: usize,
+    #[arg(short, long, default_value_t = MemorySize(0xa000_0000))]
+    memory_size: MemorySize,
 
     /// Exit when same instruction occur N time
     #[arg(long, default_value_t = 50)]
@@ -21,6 +23,76 @@ struct Args {
 
     #[arg(short, long, action = clap::ArgAction::Count)]
     verbose: u8,
+}
+
+#[derive(Debug, Clone)]
+struct MemorySize(usize);
+impl MemorySize {
+    fn to_usize(self) -> usize {
+        self.0
+    }
+
+    fn as_usize(&self) -> usize {
+        self.0
+    }
+}
+
+impl Display for MemorySize {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_usize())
+    }
+}
+
+impl From<usize> for MemorySize {
+    fn from(value: usize) -> Self {
+        Self(value)
+    }
+}
+
+impl FromStr for MemorySize {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("0x") {
+            let raw = s.trim_start_matches("0x");
+            let final_mem_size: usize = match raw.len() {
+                8 => {
+                    let result = u32::from_str_radix(raw, 16);
+                    if let Err(err) = result {
+                        return Err(format!("invalid hex string {s}: {err}"));
+                    }
+                    result
+                        .unwrap()
+                        .try_into()
+                        .expect("value is not a unsigned 32 bit value")
+                }
+                16 => {
+                    let result = u64::from_str_radix(raw, 16);
+                    if let Err(err) = result {
+                        return Err(format!("invalid hex string {s}: {err}"));
+                    }
+                    result
+                        .unwrap()
+                        .try_into()
+                        .expect("you specify a 64 bit value but your system doesn't support it")
+                }
+                _ => {
+                    return Err(format!(
+                        "fail decoding hex {} to usize: only support 32-bit or 64-bit memory size",
+                        s
+                    ))
+                }
+            };
+            Ok(MemorySize(final_mem_size))
+        } else {
+            let result: Result<usize, _> = s.parse();
+            if let Err(err) = result {
+                Err(format!("fail converting digit {} to usize: {}", s, err))
+            } else {
+                Ok(MemorySize(result.unwrap()))
+            }
+        }
+    }
 }
 
 fn main() {
@@ -42,8 +114,11 @@ fn main() {
         )
         .init();
 
-    let sim_handle =
-        SimulatorParams::to_sim_handle(args.memory_size, args.max_same_instruction, args.elf_path);
+    let sim_handle = SimulatorParams::to_sim_handle(
+        args.memory_size.to_usize(),
+        args.max_same_instruction,
+        args.elf_path,
+    );
 
     loop {
         unsafe {
