c7ba4e709e34133b2ea367924d614565aa30dfd3
Avimitin <dev@avimit.in>
2025-06-11 14:08:41 +0800

    [mill] remove riscv-opcodes from app resource

--- rvdecoderdbtest/jvm/src/Test.scala
+++ rvdecoderdbtest/jvm/src/Test.scala
@@ -2,17 +2,11 @@
 // SPDX-FileCopyrightText: 2023 Jiuyang Liu <liu@jiuyang.me>
 
 object printall extends App {
-  org.chipsalliance.rvdecoderdb.instructions(os.pwd / "rvdecoderdbtest" / "jvm" / "riscv-opcodes").foreach(println)
+  org.chipsalliance.rvdecoderdb.instructions(os.Path(sys.env("RISCV_OPCODES_PATH"))).foreach(println)
 }
 
 object json extends App {
   org.chipsalliance.rvdecoderdb
     .instructions(os.pwd / "rvdecoderdbtest" / "jvm" / "riscv-opcodes")
     .foreach(i => println(upickle.default.write(i)))
-}
-
-object fromResource extends App {
-  org.chipsalliance.rvdecoderdb
-    .instructions(org.chipsalliance.rvdecoderdb.extractResource(getClass.getClassLoader))
-    .foreach(println(_))
 }
