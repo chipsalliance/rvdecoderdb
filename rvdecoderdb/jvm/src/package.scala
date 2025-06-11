f9ba9abbfdf92056f768ab9b394900ee4d0f63d0
Avimitin <dev@avimit.in>
2025-06-11 14:13:44 +0800

    [rvdecoderdb] add custom arg_lut.csv import support

--- rvdecoderdb/jvm/src/package.scala
+++ rvdecoderdb/jvm/src/package.scala
@@ -5,41 +5,60 @@ package org.chipsalliance
 
 /** Parse instructions from riscv/riscv-opcodes */
 package object rvdecoderdb {
-  def instructions(riscvOpcodes: os.Path, custom: Iterable[os.Path] = Seq.empty): Iterable[Instruction] = {
+  def instructions(riscvOpcodes: os.Path, customOpCodes: Iterable[os.Path] = None): Iterable[Instruction] = {
+    def isInstructionSetFile(p: os.Path) = {
+      if (os.isFile(p)) {
+        val base = p.baseName
+        base.startsWith("rv128_") ||
+          base.startsWith("rv64_") ||
+          base.startsWith("rv32_") ||
+          base.startsWith("rv_")
+      } else {
+        false
+      }
+    }
+
+    val official = os
+      .walk(riscvOpcodes)
+      .filter(isInstructionSetFile)
+      .map(f => (f.baseName, os.read(f), !f.segments.contains("unratified"), false))
+
+    val custom = customOpCodes
+      .flatMap(p => {
+        if (isInstructionSetFile(p)) {
+          Seq((p.baseName, os.read(p), false, true))
+        } else {
+          os.walk(p)
+            .filter(isInstructionSetFile)
+            .map(f => (f.baseName, os.read(f), false, true))
+        }
+      })
+
     parser.parse(
-      os
-        .walk(riscvOpcodes)
-        .filter(f =>
-          f.baseName.startsWith("rv128_") ||
-            f.baseName.startsWith("rv64_") ||
-            f.baseName.startsWith("rv32_") ||
-            f.baseName.startsWith("rv_")
-        )
-        .filter(os.isFile)
-        .map(f => (f.baseName, os.read(f), !f.segments.contains("unratified"), false)) ++
-        custom
-          .flatMap(p => {
-            if (os.isFile(p)) {
-              Seq((p.baseName, os.read(p), false, true))
-            } else {
-              os.walk(p).filter(os.isFile).map(f => (f.baseName, os.read(f), false, true))
-            }
-          }),
-      argLut(riscvOpcodes).view.mapValues(a => (a.lsb, a.msb)).toMap
+      official ++ custom,
+      argLut(riscvOpcodes, customOpCodes).view.mapValues(a => (a.lsb, a.msb)).toMap
     )
   }
 
-  def argLut(riscvOpcodes: os.Path): Map[String, Arg] = os
-    .read(riscvOpcodes / "arg_lut.csv")
-    .split("\n")
-    .map { str =>
-      val l = str
-        .replace(", ", ",")
-        .replace("\"", "")
-        .split(",")
-      l(0) -> Arg(l(0), l(1).toInt, l(2).toInt)
+  def argLut(riscvOpcodes: os.Path, customOpCodes: Iterable[os.Path] = None): Map[String, Arg] = {
+    def to_args(line: String) = line.replace(", ", ",").replace("\"", "") match {
+      case s"$name,$pos0,$pos1" => name -> Arg(name, pos0.toInt, pos1.toInt)
+      case lstr                 => throw new Exception(s"invalid arg lut line: ${lstr}")
     }
-    .toMap
+
+    val official = os.read
+      .lines(riscvOpcodes / "arg_lut.csv")
+      .map(to_args)
+
+    val custom = customOpCodes
+      .flatMap(p =>
+        os.read
+          .lines(p / "arg_lut.csv")
+          .map(to_args)
+      )
+
+    (official ++ custom).toMap
+  }
 
   def causes(riscvOpcodes: os.Path): Map[String, Int] = os
     .read(riscvOpcodes / "causes.csv")
@@ -67,6 +86,7 @@ package object rvdecoderdb {
         .toMap
     )
 
+  @deprecated("extractResource is removed")
   def extractResource(cl: ClassLoader): os.Path = {
     val rvdecoderdbPath = os.temp.dir()
     val rvdecoderdbTar  = os.temp(os.read(os.resource(cl) / "riscv-opcodes.tar"))
@@ -74,6 +94,7 @@ package object rvdecoderdb {
     rvdecoderdbPath
   }
 
+  @deprecated("extractCustomResource is removed")
   def extractCustomResource(cl: ClassLoader): os.Path = {
     val rvdecoderdbPath = os.temp.dir()
     val rvdecoderdbTar  = os.temp(os.read(os.resource(cl) / "riscv-custom-opcodes.tar"))
